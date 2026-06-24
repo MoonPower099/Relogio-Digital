599e5f9cdd6fa8400530f267e5167919e9c0fadd
Lucas Saramento Brandão <lucassbbrandao123@gmail.com>
2026-06-24 17:55:33 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,19 @@ const horas = document.getElementById('horas');
 const minutos = document.getElementById('minutos');
 const segundos = document.getElementById('segundos');
 
+const gradientes = [
+    'linear-gradient(120deg, #ffe53b, #ff2525)',
+    'linear-gradient(120deg, #00c6ff, #0072ff)',
+    'linear-gradient(120deg, #833ab4, #fd1d1d, #fcb045)',
+    'linear-gradient(120deg, #11998e, #38ef7d)',
+    'linear-gradient(120deg, #fc466b, #3f5efb)',
+    'linear-gradient(120deg, #f7971e, #ffd200)'
+];
+
+const numeroAleatorio = Math.floor(Math.random() * gradientes.length);
+
+document.body.style.setProperty('--gradiente', gradientes[numeroAleatorio]);
+
 const relogio = setInterval(function time() {
     let dateToday = new Date();
     let hr = dateToday.getHours();
