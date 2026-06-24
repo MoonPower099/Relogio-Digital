d77a5e7e89d35c3c7170997a12c7d7c4f07b4ac5
Lucas Saramento Brandão <lucassbbrandao123@gmail.com>
2026-06-24 18:01:33 -0300

    Remove gradient background functionality
    Removed gradient background logic from script.js.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,6 @@ const horas = document.getElementById('horas');
 const minutos = document.getElementById('minutos');
 const segundos = document.getElementById('segundos');
 
-const gradientes = [
-    'linear-gradient(120deg, #ffe53b, #ff2525)',
-    'linear-gradient(120deg, #00c6ff, #0072ff)',
-    'linear-gradient(120deg, #833ab4, #fd1d1d, #fcb045)',
-    'linear-gradient(120deg, #11998e, #38ef7d)',
-    'linear-gradient(120deg, #fc466b, #3f5efb)',
-    'linear-gradient(120deg, #f7971e, #ffd200)'
-];
-
-const numeroAleatorio = Math.floor(Math.random() * gradientes.length);
-
-document.body.style.setProperty('--gradiente', gradientes[numeroAleatorio]);
-
 const relogio = setInterval(function time() {
     let dateToday = new Date();
     let hr = dateToday.getHours();
@@ -30,5 +17,4 @@ const relogio = setInterval(function time() {
     horas.textContent = hr;
     minutos.textContent = min;
     segundos.textContent = s;
-
-})
+});
